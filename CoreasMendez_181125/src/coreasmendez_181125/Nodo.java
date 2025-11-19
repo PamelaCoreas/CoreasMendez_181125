aa06c700b74dc6688c321f496902a458c7c82707
Pamela Cristina Coreas Mendez <ia.pamela02@ufg.edu.sv>
2025-11-18 19:51:29 -0600

    Practica 17 de noviembre del 2025 finalizada

---
+++ CoreasMendez_181125/src/coreasmendez_181125/Nodo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coreasmendez_181125;
+
+/**
+ *
+ * @author Pamela Coreas
+ */
+public class Nodo {
+       char valor;
+    int izquierda;
+    int centro;
+    int derecha;
+    
+    
+    public Nodo(char valor){
+        this.valor = valor;
+        this.izquierda = -1;
+        this.centro = -1;
+        this.derecha = -1;
+    }
+}
